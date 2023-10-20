e52269b87f0716466131424254b6887c93249b4e
Lorenzo Bernini <lorenzobernini98@gmail.com>
2023-10-20 11:24:39 +0200

    vimeo update

--- script_index.js
+++ script_index.js
@@ -1,34 +1,35 @@
 //-------------- VIMEO APPEARS -------------------
 
-const ctaButton = document.getElementById("cta-btn");
-const vimeoContainer = document.getElementById("vimeo-container");
-const overlay = document.getElementById("overlay");
-
-ctaButton.addEventListener("click", () => {
-  vimeoContainer.classList.toggle("expanded");
-  overlay.style.backgroundColor = vimeoContainer.classList.contains("expanded")
-    ? "rgba(0, 0, 0, 0.8)"
-    : "rgba(0, 0, 0, 0)";
-});
-
 // const ctaButton = document.getElementById("cta-btn");
 // const vimeoContainer = document.getElementById("vimeo-container");
 // const overlay = document.getElementById("overlay");
 
 // ctaButton.addEventListener("click", () => {
 //   vimeoContainer.classList.toggle("expanded");
-//   updateOverlay();
-// });
-
-// overlay.addEventListener("click", () => {
-//   if (vimeoContainer.classList.contains("expanded")) {
-//     vimeoContainer.classList.remove("expanded");
-//     updateOverlay();
-//   }
-// });
-
-// function updateOverlay() {
 //   overlay.style.backgroundColor = vimeoContainer.classList.contains("expanded")
 //     ? "rgba(0, 0, 0, 0.8)"
 //     : "rgba(0, 0, 0, 0)";
-// }
+// });
+
+const ctaButton = document.getElementById("cta-btn");
+const vimeoContainer = document.getElementById("vimeo-container");
+const overlay = document.getElementById("overlay");
+
+ctaButton.addEventListener("click", (event) => {
+  event.stopPropagation(); // Prevent triggering overlay's click event
+  toggleVimeo();
+});
+
+overlay.addEventListener("click", () => {
+  if (vimeoContainer.classList.contains("expanded")) {
+    toggleVimeo();
+  }
+});
+
+function toggleVimeo() {
+  vimeoContainer.classList.toggle("expanded");
+
+  const isExpanded = vimeoContainer.classList.contains("expanded");
+  overlay.style.backgroundColor = isExpanded ? "rgba(0, 0, 0, 0.8)" : "rgba(0, 0, 0, 0)";
+  overlay.style.cursor = isExpanded ? "zoom-out" : "default";
+}

850041588b30a0353a9d2eb0f6049bc4788c01c5
Lorenzo Bernini <lorenzobernini98@gmail.com>
2025-06-10 10:04:43 +0200

    credit fix

--- script_index.js
+++ script_index.js
@@ -1,6 +1,7 @@
 const ctaButton = document.getElementById("cta");
 const vimeoContainer = document.getElementById("vimeo-container");
 const overlay = document.getElementById("overlay");
+const credit = document.getElementById("credit");
 
 // Store the YouTube embed URL
 const youtubeURL =
@@ -27,7 +28,9 @@ function toggleVimeo() {
   // Add or remove the iframe to start/stop the video
   if (isExpanded) {
     vimeoContainer.innerHTML = `<iframe width="100%" height="100%" src="${youtubeURL}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+    credit.style.opacity = 0; // Hide the credit when expanded
   } else {
     vimeoContainer.innerHTML = "";
+    credit.style.opacity = 1; // Show the credit when collapsed
   }
 }
